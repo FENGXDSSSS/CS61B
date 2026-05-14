2217c46b7ea548bb30811338f48818ee398cc8ea
FENGXDSSSS <1557499466@qq.com>
2026-05-14 18:00:35 +0800

    Update MaxArrayDeque.java

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,17 +1,17 @@
 package deque;
 import java.util.Comparator;
-public class MaxArrayDeque<Item> extends ArrayDeque<Item> {
-    public MaxArrayDeque(Comparator<Item> c){
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    public MaxArrayDeque(Comparator<T> c){
         comp = c;
     }
 
-    public Item max(){
+    public T max(){
         if (size == 0){
             return null;
         }
-        Item max_item = get(0);
+        T max_item = get(0);
         for (int i = 1; i < size; i++){
-            Item temp = get(i);
+            T temp = get(i);
             if (comp.compare(temp, max_item) > 0){
                 max_item = temp;
             }
@@ -19,13 +19,13 @@ public class MaxArrayDeque<Item> extends ArrayDeque<Item> {
         return max_item;
     }
 
-    public Item max(Comparator<Item> c){
+    public T max(Comparator<T> c){
         if (size == 0){
             return null;
         }
-        Item max_item = get(0);
+        T max_item = get(0);
         for (int i = 1; i < size; i++){
-            Item temp = get(i);
+            T temp = get(i);
             if (c.compare(temp, max_item) > 0){
                 max_item = temp;
             }
@@ -33,5 +33,5 @@ public class MaxArrayDeque<Item> extends ArrayDeque<Item> {
         return max_item;
     }
 
-    private Comparator<Item> comp;
+    private Comparator<T> comp;
 }
