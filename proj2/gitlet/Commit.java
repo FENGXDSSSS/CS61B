d956d9a3f2efcc2f1b63c48590c771c57df0945f
FENGXDSSSS <1557499466@qq.com>
2026-05-21 21:39:27 +0800

    Update Commit.java

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -21,6 +21,7 @@ public class Commit {
 
     /** The message of this Commit. */
     private String message;
-
+    private Commit left;
+    private Commit right;
     /* TODO: fill in the rest of this class. */
 }
--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -13,12 +13,17 @@ public class Main {
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
-                // TODO: handle the `init` command
+                Repository.init();
+
                 break;
             case "add":
-                // TODO: handle the `add [filename]` command
+                Repository.add(args[0]);
                 break;
-            // TODO: FILL THE REST IN
+            case "commit":
+
+                break;
+            case "":
+
         }
     }
 }
